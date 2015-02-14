312015fd39c624d73294171de9795841b600412c
Tamara Tanasescu <gokimster@yahoo.com>
2015-02-14 16:26:03 +0000

    Apple Layers for PopUps

--- Assets/Scripts/TargetSpawnManager.cs
+++ Assets/Scripts/TargetSpawnManager.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class TargetSpawnManager : MonoBehaviour {
 
-	public GameObject appleTemplate;
+	public GameObject appleTemplate1;
+	public GameObject appleTemplate2;
 	public GameObject flyingAppleTemplate;
 
     public GameObject burgerTemplate;
@@ -17,12 +18,15 @@ public class TargetSpawnManager : MonoBehaviour {
 	public float flyingSpawnInterval;
 
 
+	private GameObject[] spawnPoints1;
+	private GameObject[] spawnPoints2;
 	private GameObject[] spawnPoints;
 	private float popUpCountDown;
 
 	// Use this for initialization
 	void Start () {
-		spawnPoints = GameObject.FindGameObjectsWithTag ("SpawnPoint");
+		spawnPoints1 = GameObject.FindGameObjectsWithTag ("SpawnPoint1");
+		spawnPoints2 = GameObject.FindGameObjectsWithTag ("SpawnPoint2");
 		//print ("No of spawnPoints: " + spawnPoints.Length);
 		popUpCountDown = popUpSpawnInterval;
 	}
@@ -39,6 +43,14 @@ public class TargetSpawnManager : MonoBehaviour {
 		
 		if (popUpCountDown <= 0) {
 			popUpCountDown = popUpSpawnInterval;
+			int layerNo=Random.Range(1,3);
+			if (layerNo == 1)
+			{
+				spawnPoints = spawnPoints1;
+			}else
+			{
+				spawnPoints = spawnPoints2;
+			}
 			int spawnPointNo = Random.Range (0,spawnPoints.Length);
             switch(Random.Range(1,5))
             {
@@ -49,8 +61,12 @@ public class TargetSpawnManager : MonoBehaviour {
                     GameObject burgerClone = (GameObject)Instantiate(burgerTemplate, spawnPoints[spawnPointNo].transform.position, Quaternion.identity);
                     break;
                 case 3: case 4: case 5:
-                    GameObject appleClone = (GameObject)Instantiate(appleTemplate, spawnPoints[spawnPointNo].transform.position, Quaternion.identity);
-                    break;
+					if (layerNo == 1) {
+                    GameObject appleClone = (GameObject)Instantiate(appleTemplate1, spawnPoints[spawnPointNo].transform.position, Quaternion.identity);
+					}else{
+					GameObject appleClone = (GameObject)Instantiate(appleTemplate2, spawnPoints[spawnPointNo].transform.position, Quaternion.identity);
+					}
+					break;
                 default:
                     break;
             }
