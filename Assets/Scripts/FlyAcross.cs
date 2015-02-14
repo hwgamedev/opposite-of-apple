c1001c3f38e10093d252941e3cb23da569144c82
Tamara Tanasescu <gokimster@yahoo.com>
2015-02-14 14:33:41 +0000

    Flying Apples!

---
+++ Assets/Scripts/FlyAcross.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyAcross : MonoBehaviour {
+
+
+	public float verticalForce = 150f;
+	public float horizontalForce = 500f;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddForce(new Vector2(horizontalForce, verticalForce));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SingleArc ()
+	{
+		rigidbody2D.AddForce(new Vector2(horizontalForce, verticalForce));
+	}
+}
