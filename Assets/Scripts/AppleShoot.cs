99c04d52c9d1a6851342face883bac3d666de70c
John Scott <johnz0r.steam@gmail.com>
2015-02-14 12:24:15 +0000

    Chunks and Explode

--- Assets/Scripts/AppleShoot.cs
+++ Assets/Scripts/AppleShoot.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class AppleShoot : MonoBehaviour {
+    public GameObject chunk1;
+    public GameObject chunk2;
+    public GameObject chunk3;
+    public GameObject chunk4;
+    public GameObject chunk5;
+    public GameObject chunk6;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +20,32 @@ public class AppleShoot : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
+
+        GameObject applechunk1 = Instantiate(chunk1, new Vector3(Random.Range(0, 1), Random.Range(0, 1), 0.0f), Quaternion.identity) as GameObject;
+        applechunk1.rigidbody2D.AddForce(Vector3.right * Random.Range(-50, 50));
+        applechunk1.rigidbody2D.AddForce(Vector3.up * Random.Range(100, 400));
+
+        GameObject applechunk2 = Instantiate(chunk2, new Vector3(Random.Range(0, 1), Random.Range(0, 1), 0.0f), Quaternion.identity) as GameObject;
+        applechunk2.rigidbody2D.AddForce(Vector3.right * Random.Range(-50, 50));
+        applechunk2.rigidbody2D.AddForce(Vector3.up * Random.Range(100, 400));
+
+        GameObject applechunk3 = Instantiate(chunk3, new Vector3(Random.Range(0, 1), Random.Range(0, 1), 0.0f), Quaternion.identity) as GameObject;
+        applechunk3.rigidbody2D.AddForce(Vector3.right * Random.Range(-50, 50));
+        applechunk3.rigidbody2D.AddForce(Vector3.up * Random.Range(100, 400));
+
+        GameObject applechunk4 = Instantiate(chunk4, new Vector3(Random.Range(0, 1), Random.Range(0, 1), 0.0f), Quaternion.identity) as GameObject;
+        applechunk4.rigidbody2D.AddForce(Vector3.right * Random.Range(-50, 50));
+        applechunk4.rigidbody2D.AddForce(Vector3.up * Random.Range(100, 400));
+
+        GameObject applechunk5 = Instantiate(chunk5, new Vector3(Random.Range(0, 1), Random.Range(0, 1), 0.0f), Quaternion.identity) as GameObject;
+        applechunk5.rigidbody2D.AddForce(Vector3.right * Random.Range(-50, 50));
+        applechunk5.rigidbody2D.AddForce(Vector3.up * Random.Range(100, 400));
+
+        GameObject applechunk6 = Instantiate(chunk6, new Vector3(Random.Range(0, 1), Random.Range(0, 1), 0.0f), Quaternion.identity) as GameObject;
+        applechunk6.rigidbody2D.AddForce(Vector3.right * Random.Range(-50, 50));
+        applechunk6.rigidbody2D.AddForce(Vector3.up * Random.Range(100, 400));
+
 		Destroy (gameObject);
+
 	}
 }
---
+++ Assets/Scripts/DestroyChunks.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyChunks : MonoBehaviour {
+    bool seen = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (renderer.isVisible) seen = true;
+        if (seen && !renderer.isVisible)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
